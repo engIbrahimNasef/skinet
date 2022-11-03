1b5adf45901e5189c4a4feb1556820df5e653c11
UI_Ibrahim Aly Elmetwaly Nasef <unityinfotech_nasef@DSC.gov.ae>
2022-11-03 13:31:26 +0400

    end of section 8

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { IPagination } from './modules/pagination';
+import { IProduct } from './modules/product';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  
   title = 'Skinet';
+  products:IProduct[];
+  product:IProduct;
+  /**
+   *
+   */
+  constructor(private http: HttpClient) {
+    
+  }
+
+  ngOnInit(): void{ 
+    this.http.get('https://localhost:5001/api/products?pageSize=50').subscribe((response: IPagination)=>{
+      this.products = response.data;
+    },error => {
+      console.log(error);
+    })
+  }
 }
